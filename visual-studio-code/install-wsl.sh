9650a0b55752e3224f69307fe5d6c50869dd3dd8
JF1HNL <kimossan88@gmail.com>
2021-10-09 14:25:44 +0900

    add : wsl側からシンボリックリンクを追加する

---
+++ visual-studio-code/install-wsl.sh
@@ -0,0 +1,19 @@
+cd /mnt/c/Users/JF1HN/AppData/Roaming/Code/User
+
+# 前の設定をバックアップ
+mv settings.json settings.json.bak
+mv keybindings.json keybindings.json.bak
+mv snippets snippets.bak
+
+# ~/dotfiles/vscode以下に他マシンと共有したい設定ファイルが置かれている想定
+# 自分はこのdotfilesをgithubで管理している
+#
+# 元の設定ファイルの代わりにシンボリックリンクを置いておく
+ln -sf ~/dotfiles/visual-studio-code/settings.json settings.json
+ln -sf ~/dotfiles/visual-studio-code/keybindings.json keybindings.json
+ln -sf ~/dotfiles/visual-studio-code/snippets snippets
+
+# 他マシンで書き出されたエクステンション一覧を使ってインストール
+for extension in `cat ~/dotfiles/visual-studio-code/extensions.txt`; do
+  code --install-extension $extension
+done
